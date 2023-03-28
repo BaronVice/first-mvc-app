a141e28fde1aeccf017640750992386e8984d44f
BaronVice <toni.sas.228@mail.ru>
2023-03-28 22:20:10 +0800

    Calculator + use of get parameters + send model from controller to view

--- src/main/java/baronvice/mvcstuff/controllers/FirstController.java
+++ src/main/java/baronvice/mvcstuff/controllers/FirstController.java
@@ -1,20 +1,59 @@
 package baronvice.mvcstuff.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/cool-page")
 public class FirstController {
 
     @GetMapping("/entrance")
-    public String entrancePage(){
+    public String entrancePage(@RequestParam(value = "name", required = false) String name,
+                               @RequestParam(value = "surname", required = false) String surname,
+                               Model model){
+
+        if (name == null || surname == null){
+            name = "Unknown";
+            surname = "User";
+        }
+
+        model.addAttribute("message", String.format("Hello, %s %s!", name, surname));
+
         return "first/entrance";
     }
 
     @GetMapping("/exit")
     public String exitPage(){
         return "first/exit";
+    }
+
+    @GetMapping("/calc")
+    public String calcPage(@RequestParam("a") String a,
+                           @RequestParam("b") String b,
+                           @RequestParam("op") String op,
+                           Model model){
+
+        try{
+            int first = Integer.parseInt(a);
+            int second = Integer.parseInt(b);
+            String operation;
+            int result;
+            switch (op){
+                case "add" -> { result = first + second; operation = "+"; }
+                case "sub" -> { result = first - second; operation = "-"; }
+                case "mul" -> { result = first * second; operation = "*"; }
+                case "div" -> { result = first / second; operation = "/"; }
+                default -> throw new RuntimeException();
+            }
+            model.addAttribute("message", String.format("%d %s %d = %d", first, operation, second, result));
+        }
+        catch (Exception e){
+            model.addAttribute("message", "Dumb ass...");
+        }
+
+        return "first/calculator";
     }
 }
